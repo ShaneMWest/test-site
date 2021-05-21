94cd6fe0119a2c08072a2a3fe1e4574e616b80d7
Shane-M-West <shane.martin.west@gmail.com>
2021-05-21 11:16:31 -0400

    Forced user name input

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,29 @@ myImage.onclick = function(){
         myImage.setAttribute('src','images/clearance-items-banner-1.jpg');
     }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+    let myName = prompt('Please enter your name.');
+    if(!myName){
+        setUserName();
+    } else {
+        localStorage.setItem('name',myName);
+        myHeading.textContent = 'Welcome to Atlas Tools and Machinery, ' + myName + '!';
+    }
+}
+
+//When there is no user (e.g. at first site access), prompts for user name entry.
+if(!localStorage.getItem('name')){
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Welcome to Atlas Tools and Machinery, ' + storedName + '!';
+}
+
+//On clicking the button, prompts user to enter their name.
+myButton.onclick = function() {
+    setUserName();
+}
