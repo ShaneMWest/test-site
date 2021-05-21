4d6db24a5f5757270a4cc372300e49f34b97807e
Shane-M-West <shane.martin.west@gmail.com>
2021-05-21 08:44:26 -0400

    Enabling scripting on page

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+/*
+First script
+Still learning
+*/
+
+let myVariable;
+myVariable = 'Bob';
+
+//Comment for practice
+let myVar2 = 'Andy';
+
+
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
